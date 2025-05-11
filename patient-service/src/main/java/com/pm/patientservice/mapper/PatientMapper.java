54c9fa290a9bf5f87362b852facbd6e419cbe1e8
pspawar08 <pruthvirajpawar30@gmail.com>
2025-05-11 18:09:59 +0530

    PatientRequestDTo  obj to Patient obj
    conversion of PatientRequestDTO to Patient Entity object

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -18,4 +18,14 @@ public class PatientMapper {
         return patientDTO;
     }
 
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
+    }
+
 }

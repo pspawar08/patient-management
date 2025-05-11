bf23fbef383dadc815ba0646077323d02018a452
pspawar08 <pruthvirajpawar30@gmail.com>
2025-05-11 18:07:34 +0530

    Created Mapper

---
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,10 @@
+package com.pm.patientservice.mapper;
+
+import com.pm.patientservice.dto.PatientRequestDTO;
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+
+import java.time.LocalDate;
+
+public class PatientMapper {
+}

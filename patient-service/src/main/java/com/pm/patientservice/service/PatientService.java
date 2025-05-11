fc0cecf53204965f1952a8c5482efc9f26ac2be3
pspawar08 <pruthvirajpawar30@gmail.com>
2025-05-11 18:10:36 +0530

    createPatient service to add new patient

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -32,3 +32,11 @@ public class PatientService {
                 .map(PatientMapper::toDTO).toList();
 
     }
+
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+
+        return PatientMapper.toDTO(newPatient);
+    }
+}

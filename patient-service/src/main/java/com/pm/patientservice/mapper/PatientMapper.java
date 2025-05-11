2fced50b27b801a2384eb0bf71672db8341b3efd
pspawar08 <pruthvirajpawar30@gmail.com>
2025-05-11 18:09:05 +0530

    Patinet obj to PatientResponseDTO obj
    conversion of Patient Entity object to PatientResponseDTO

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -7,4 +7,15 @@ import com.pm.patientservice.model.Patient;
 import java.time.LocalDate;
 
 public class PatientMapper {
+
+    public static PatientResponseDTO toDTO(Patient patient) {
+        PatientResponseDTO patientDTO = new PatientResponseDTO();
+        patientDTO.setId(patient.getId().toString());
+        patientDTO.setName(patient.getName().toString());
+        patientDTO.setAddress(patient.getAddress().toString());
+        patientDTO.setEmail(patient.getEmail().toString());
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+        return patientDTO;
+    }
+
 }

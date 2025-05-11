cc1d3019ff936b9db48fc88d2313d43626bedb34
pspawar08 <pruthvirajpawar30@gmail.com>
2025-05-11 18:01:04 +0530

    Create Patient Service

---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,19 @@
+package com.pm.patientservice.service;
+
+import com.pm.patientservice.dto.PatientRequestDTO;
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PatientService {
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }

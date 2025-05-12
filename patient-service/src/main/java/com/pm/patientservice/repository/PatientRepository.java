7dd052de0810ff898e7fda3b8cf31d3f0bd1f397
pspawar08 <pruthvirajpawar30@gmail.com>
2025-05-12 16:43:10 +0530

    Update PatientRepository.java

--- patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
+++ patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
@@ -2,12 +2,16 @@ package com.pm.patientservice.repository;
 
 
 import com.pm.patientservice.model.Patient;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, UUID> {
+    boolean existsByEmail(String email);
 
 }
